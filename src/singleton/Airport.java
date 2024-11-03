2ff215db008271d023b623df7c01882c29c013cf
Muhammad Raees Ahmed <muhammadraeesahmed38@gmail.com>
2024-11-03 09:06:00 -0800

    make Airport singleton class

---
+++ src/singleton/Airport.java
@@ -0,0 +1,21 @@
+package singleton;
+
+public class Airport {
+
+    private static Airport airport = null;
+
+    // make private constructor
+    private Airport() {
+
+    }
+
+    // make static method that return object/instance of Airport Class
+    public static Airport getAirport() {
+
+        if (airport == null) {
+            airport = new Airport();
+        }
+
+        return airport;
+    }
+}
