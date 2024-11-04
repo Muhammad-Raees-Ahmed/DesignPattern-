0d03a59f367bc972929d09b6bdf70a885ee13406
Muhammad Raees Ahmed <muhammadraeesahmed38@gmail.com>
2024-11-04 08:14:44 -0800

    new point added.

--- src/singleton/SeaPort.java
+++ src/singleton/SeaPort.java
@@ -2,7 +2,8 @@ package singleton;
 
 public class SeaPort {
 
-    // eager way of singleton object creation / early instantiation  . If the object of this SeaPort don't need to use in entire application so this object creation is useless because we don't need to use this object but it takes memory.
+    /*eager way of singleton object creation / early instantiation  . If the object of this SeaPort don't need to use in entire application so this object creation is useless because we don't need to use this object but it takes memory.
+    both approaches have merits and demerits so it depends on our requirements.*/
     private static SeaPort seaPort=new SeaPort();
 
     public static SeaPort getSeaPort(){
