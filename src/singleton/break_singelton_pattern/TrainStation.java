c5e060150b42956a78e8f365acfae99d245e1bfb
Muhammad Raees Ahmed <muhammadraeesahmed38@gmail.com>
2024-11-04 08:31:54 -0800

    techniques of singleton breakage

---
+++ src/singleton/break_singelton_pattern/TrainStation.java
@@ -0,0 +1,13 @@
+package singleton.break_singelton_pattern;
+
+
+/*we can break the singleton class by following techniques
+
+1) By Using Reflection API
+2) By Using Deserialization
+3) By Cloning the object.
+
+All these techniques break the singleton class/rule  
+*/
+public class TrainStation {
+}
