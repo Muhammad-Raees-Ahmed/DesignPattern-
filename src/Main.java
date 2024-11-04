4fc433932f4a8621eb36621a013571db82cc3146
Muhammad Raees Ahmed <muhammadraeesahmed38@gmail.com>
2024-11-04 09:10:53 -0800

    All 3 approaches of break the singleton class done

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
 import singleton.Airport;
 import singleton.SeaPort;
+import singleton.break_singelton_pattern.TrainStation;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.lang.reflect.Constructor;
 
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception,CloneNotSupportedException {
 
 
         // lazy instantiation
@@ -20,5 +27,39 @@ public class Main {
 
         System.out.println("hashCode of seaPort1 : "+seaPort1.hashCode());
         System.out.println("hashCode of seaPort2 : "+seaPort2.hashCode());
-    }
+
+        System.out.println("-------------Approch 1--------------");
+
+        // Break Singleton Class By Reflection Api
+
+        TrainStation trainStation1=TrainStation.getTrainStation();
+        System.out.println("hasCode of trainStation1 :" +trainStation1.hashCode());
+        Constructor<TrainStation> constructor=TrainStation.class.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        TrainStation trainStation2=constructor.newInstance();
+        System.out.println("hasCode of trainStation2 :" +trainStation2.hashCode());
+
+        System.out.println("-------------Approch 2--------------");
+        // Break Singleton By Deserialization
+
+        // serialize the obj
+        TrainStation trainStation3=TrainStation.getTrainStation();
+        System.out.println("hasCode of tranStation3 :"+trainStation3.hashCode());
+        ObjectOutputStream objectOutputStream=new ObjectOutputStream(new FileOutputStream("dummy.ob"));
+        objectOutputStream.writeObject(trainStation3);
+
+        // deSerialize the obj
+        ObjectInputStream objectInputStream=new ObjectInputStream(new FileInputStream("dummy.ob"));
+        TrainStation trainStation4=(TrainStation) objectInputStream.readObject();
+        System.out.println("hasCode of tranStation4 :"+trainStation4.hashCode());
+
+        System.out.println("---------------Approch 3---------------");
+
+        TrainStation trainStation5=TrainStation.getTrainStation();
+        System.out.println("hasCode of tranStation5 :"+trainStation5.hashCode());
+        TrainStation trainStation6=trainStation5.clone();
+        System.out.println("hasCode of tranStation6 :"+trainStation6.hashCode());
+
+
+     }
 }
--- src/singleton/break_singelton_pattern/TrainStation.java
+++ src/singleton/break_singelton_pattern/TrainStation.java
@@ -1,13 +1,41 @@
 package singleton.break_singelton_pattern;
 
 
+import java.io.Serializable;
+
 /*we can break the singleton class by following techniques
 
-1) By Using Reflection API
-2) By Using Deserialization
+1) Problem : By Using Reflection API
+   Solution (Use Enum, check if obj is created and try to make another obj so return runtime exception)
+2) Problem : By Using Deserialization
+   Solution : Implementing readResolve() method
 3) By Cloning the object.
 
-All these techniques break the singleton class/rule  
+All these techniques break the singleton class/rule
 */
-public class TrainStation {
+public class TrainStation implements Serializable,Cloneable {
+
+    private static TrainStation trainStation=null;
+
+    private TrainStation() {
+
+    }
+
+    public static TrainStation getTrainStation(){
+        if(trainStation==null){
+            trainStation = new TrainStation();
+        }
+
+        return trainStation;
+    }
+
+    @Override
+    public TrainStation clone() {
+        try {
+            TrainStation clone = (TrainStation) super.clone();
+            return clone;
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
+    }
 }
